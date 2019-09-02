fce81a5f644c3fc0a5d509800280de184699017f
Juan David Alarcon <jd.alarcons@uniandes.edu.co>
2019-09-02 13:20:04 -0500

    Agregados metodos y pruebas de persistencia para Calificacion, ViajeRecurrente y Vehiculo

--- s1_carpooling-back/src/test/java/co/edu/uniandes/csw/carpooling/test/persistence/VehiculoPersistanceTest.java
+++ s1_carpooling-back/src/test/java/co/edu/uniandes/csw/carpooling/test/persistence/VehiculoPersistanceTest.java
@@ -6,15 +6,20 @@
 package co.edu.uniandes.csw.carpooling.test.persistence;
 
 import co.edu.uniandes.csw.carpooling.entities.VehiculoEntity;
+import co.edu.uniandes.csw.carpooling.entities.VehiculoEntity;
 import co.edu.uniandes.csw.carpooling.persistence.VehiculoPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -41,6 +46,43 @@ public class VehiculoPersistanceTest {
 
     @PersistenceContext(unitName = "carpoolingPU")
     protected EntityManager em;
+   
+    @Inject
+    UserTransaction utx;
+  
+    private List<VehiculoEntity> data = new ArrayList<VehiculoEntity>();
+
+    @Before
+    public void setUp() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private void clearData() {
+        em.createQuery("delete from VehiculoEntity").executeUpdate();
+    }
+
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            VehiculoEntity entity = factory.manufacturePojo(VehiculoEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
 
     @Test
     public void createTest() {
@@ -54,5 +96,52 @@ public class VehiculoPersistanceTest {
         VehiculoEntity entity = em.find(VehiculoEntity.class, result.getId());
         Assert.assertEquals(vehiculo.getSoat(), entity.getSoat());
     }
+    
+     @Test
+    public void getVehiculosTest() {
+        List<VehiculoEntity> list = cp.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (VehiculoEntity ent : list) {
+            boolean found = false;
+            for (VehiculoEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+        @Test
+    public void getVehiculoTest() {
+        VehiculoEntity entity = data.get(0);
+        VehiculoEntity newEntity = cp.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getId(), newEntity.getId());
+    }
+    
+     @Test
+    public void updateVehiculoTest() {
+        VehiculoEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        VehiculoEntity newEntity = factory.manufacturePojo(VehiculoEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        cp.update(newEntity);
+
+        VehiculoEntity resp = em.find(VehiculoEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getId(), resp.getId());
+    }
+    
+     @Test
+    public void deleteVehiculoTest() {
+        VehiculoEntity entity = data.get(0);
+        cp.delete(entity.getId());
+        VehiculoEntity deleted = em.find(VehiculoEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+
 
 }
--- s1_carpooling-back/src/test/java/co/edu/uniandes/csw/carpooling/test/persistence/ViajeRecurrentePersistanceTest.java
+++ s1_carpooling-back/src/test/java/co/edu/uniandes/csw/carpooling/test/persistence/ViajeRecurrentePersistanceTest.java
@@ -6,15 +6,20 @@
 package co.edu.uniandes.csw.carpooling.test.persistence;
 
 import co.edu.uniandes.csw.carpooling.entities.ViajeRecurrenteEntity;
+import co.edu.uniandes.csw.carpooling.entities.ViajeRecurrenteEntity;
 import co.edu.uniandes.csw.carpooling.persistence.ViajeRecurrentePersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -43,6 +48,47 @@ public class ViajeRecurrentePersistanceTest {
 
      @PersistenceContext(unitName= "carpoolingPU")
      protected EntityManager em;
+     
+     
+     
+        
+    @Inject
+    UserTransaction utx;
+  
+    private List<ViajeRecurrenteEntity> data = new ArrayList<ViajeRecurrenteEntity>();
+
+    @Before
+    public void setUp() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private void clearData() {
+        em.createQuery("delete from ViajeRecurrenteEntity").executeUpdate();
+    }
+
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            ViajeRecurrenteEntity entity = factory.manufacturePojo(ViajeRecurrenteEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+
     @Test
     public void  createTest()
     {
@@ -56,6 +102,53 @@ public class ViajeRecurrentePersistanceTest {
         
         ViajeRecurrenteEntity entity = em.find(ViajeRecurrenteEntity.class, result.getId());
         Assert.assertEquals(viajeRecurrente.getFrecuencia(), entity.getFrecuencia());
+    }
+    
+         @Test
+    public void getViajesRecurrentesTest() {
+        List<ViajeRecurrenteEntity> list = cp.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (ViajeRecurrenteEntity ent : list) {
+            boolean found = false;
+            for (ViajeRecurrenteEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    
+    @Test
+    public void getViajeRecurrenteTest() {
+        ViajeRecurrenteEntity entity = data.get(0);
+        ViajeRecurrenteEntity newEntity = cp.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getId(), newEntity.getId());
+    }
+    
+     @Test
+    public void updateViajeRecurrenteTest() {
+        ViajeRecurrenteEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        ViajeRecurrenteEntity newEntity = factory.manufacturePojo(ViajeRecurrenteEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        cp.update(newEntity);
+
+        ViajeRecurrenteEntity resp = em.find(ViajeRecurrenteEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getId(), resp.getId());
+    }
+    
+     @Test
+    public void deleteViajeRecurrenteTest() {
+        ViajeRecurrenteEntity entity = data.get(0);
+        cp.delete(entity.getId());
+        ViajeRecurrenteEntity deleted = em.find(ViajeRecurrenteEntity.class, entity.getId());
+        Assert.assertNull(deleted);
     }
     
 }
