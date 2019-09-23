40d0b58e299f72deaf521b740ee1d152ef7fe76a
Santiago <s.ballesteros@uniandes.edu.co>
2019-09-23 10:30:12 -0500

    Publicista Logic y Viajero Logic implementado

--- s1_carpooling-back/src/test/java/co/edu/uniandes/csw/carpooling/test/logic/PublicistaLogicTest.java
+++ s1_carpooling-back/src/test/java/co/edu/uniandes/csw/carpooling/test/logic/PublicistaLogicTest.java
@@ -29,7 +29,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
- * @author SantiagoBallesteros
+ * @author Santiago Ballesteros
  */
 @RunWith(Arquillian.class)
 public class PublicistaLogicTest {
