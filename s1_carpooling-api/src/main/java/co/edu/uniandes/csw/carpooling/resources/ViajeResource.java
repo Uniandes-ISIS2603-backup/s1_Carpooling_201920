b66661df53e82a3bd70d2b34ce8fa793664ed03f
Juan David Serrano <jd.serranom@uniandes.edu.co>
2019-09-24 20:13:01 -0500

    Se crearon DTO's vacios Viaje y Trayecto y Resource solo con POST

---
+++ s1_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/resources/ViajeResource.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.carpooling.resources;
+
+import co.edu.uniandes.csw.carpooling.dtos.ViajeDTO;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author Estudiante
+ */
+@Path("viajes")
+@Produces("aplication/json")
+@Consumes("aplication/json")
+@RequestScoped
+public class ViajeResource {
+    
+    private static final Logger LOGGER = Logger.getLogger(ViajeResource.class.getName());
+    
+    @POST
+    public ViajeDTO createViaje(ViajeDTO viaje){
+        return viaje;
+    }
+    
+}
