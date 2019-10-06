1d06e593aa78faa131ffa86c84c0632ed888b948
Juan David <jd.serranom@uniandes.edu.co>
2019-10-05 20:53:44 -0500

    Se generaron las pruebas de postman para Viaje y Trayecto

--- s1_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/dtos/ViajeDetailDTO.java
+++ s1_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/dtos/ViajeDetailDTO.java
@@ -11,9 +11,55 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
+ * Clase que extiende de {@link ViajeDTO} para manejar las relaciones entre
+ * los Viaje JSON y otros DTOs. Para conocer el contenido de un
+ * Viaje vaya a la documentacion de {@link ViajeDTO}
  *
- * @author Estudiante
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "id": Long,
+ *      "origen": String,
+ *      "destino": String,
+ *      "fechaDeSalida": Date,
+ *      "fechaDeLlegada": Date,
+ *      "cupos": Integer,
+ *      "costoViaje": Double,
+ *      "vehiculo": String,
+ *      "estadoViaje: ESTADO_DE_VIAJE,
+ *      "trayectos": [{@link TrayectoDTO}]
+ *   }
+ * </pre> Por ejemplo una editorial se representa asi:<br>
+ *
+ * <pre>
+ *
+ *   {
+ *      "id":1,
+ *      "destino": "Bucaramanga",
+ *      "origen": "Bogota",
+ *      "fechaDeSalida": "2019-11-03",
+ *      "fechaDeLlegada": "2019-11-03",
+ *      "cupos": 2,
+ *      "costoViaje": 30000.0,
+ *      "vehiculo":"DBS594",
+ *      "estadoViaje": "PUBLICADO",
+ *      "trayectos" : [
+ *          {
+ *              "id" : 1,
+ *              "numPeajes" :3,
+ *              "duracion": 120,
+ *              "costoCombustible" : 50000,
+ *              "origen" : "Calle 113 #40-25",
+ *              "destino" : "Transversal 67 #170-15"
+ *          }
+ *      ]
+ *   }
+ *
+ * </pre>
+ *
+ * @author Juan David Serrano
  */
 public class ViajeDetailDTO extends ViajeDTO implements Serializable{
     
--- s1_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/resources/ViajeResource.java
+++ s1_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/resources/ViajeResource.java
@@ -17,18 +17,23 @@ import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author Estudiante
  */
-@Path("viajes")
-@Produces("aplication/json")
-@Consumes("aplication/json")
+@Path("/viajes")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 @RequestScoped
 public class ViajeResource {
     
@@ -38,7 +43,7 @@ public class ViajeResource {
     private ViajeLogic logic;
     
     @POST
-    public ViajeDTO createViaje(ViajeDTO viaje) throws BusinessLogicException{
+    public ViajeDTO createViaje(ViajeDetailDTO viaje) throws BusinessLogicException{
         LOGGER.log(Level.INFO, "ViajeResource createViaje: input: {0}", viaje);
         ViajeEntity viajeEntity =viaje.toEntity();
         viajeEntity = logic.createViaje(viajeEntity);
@@ -48,9 +53,64 @@ public class ViajeResource {
     
     @GET
     public List<ViajeDetailDTO> getViajes(){
+        LOGGER.info("ViajeResource getViajes: input: void");
         List<ViajeDetailDTO> viajes = listViajesEntityToDTO(logic.getViajes());
+        LOGGER.log(Level.INFO, "ViajeResource getViajes: output: {0}", viajes);
         return viajes;
     } 
+    
+    
+    @GET
+    @Path("{viajesId: \\d+}")
+    public ViajeDetailDTO getViaje(@PathParam("viajesId") Long viajesId) {
+        LOGGER.log(Level.INFO, "ViajeResource getViaje: input: {0}", viajesId);
+        ViajeEntity viajeEntity = logic.getViaje(viajesId);
+        if (viajeEntity == null) {
+            throw new WebApplicationException("El recurso /viajes/" + viajesId + " no existe.", 404);
+        }
+        ViajeDetailDTO detailDTO = new ViajeDetailDTO(viajeEntity);
+        LOGGER.log(Level.INFO, "ViajeResource getViaje: output: {0}", detailDTO);
+        return detailDTO;
+    }
+    
+    
+    @PUT
+    @Path("{viajesId: \\d+}")
+    public ViajeDetailDTO updateViaje(@PathParam("viajesId") Long viajesId, ViajeDetailDTO viaje) throws BusinessLogicException{
+        LOGGER.log(Level.INFO, "ViajeResource updateViaje: input: viajesId: {0} , author: {1}", new Object[]{viajesId, viaje});
+        viaje.setId(viajesId);
+        if (logic.getViaje(viajesId) == null) {
+            throw new WebApplicationException("El recurso /viajes/" + viajesId + " no existe.", 404);
+        }
+        ViajeDetailDTO detailDTO = new ViajeDetailDTO(logic.updateViaje(viajesId, viaje.toEntity()));
+        LOGGER.log(Level.INFO, "ViajeResource updateViaje: output: {0}", detailDTO);
+        return detailDTO;
+    }
+    
+    
+    @DELETE
+    @Path("{viajesId: \\d+}")
+    public void deleteViaje(@PathParam("viajesId") Long viajesId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "ViajeResource deleteViaje: input: {0}", viajesId);
+        if (logic.getViaje(viajesId) == null) {
+            throw new WebApplicationException("El recurso /viajes/" + viajesId + " no existe.", 404);
+        }
+        logic.deleteViaje(viajesId);
+        LOGGER.info("ViajeResource deleteViaje: output: void");
+    }
+    
+    
+    @Path("{viajesId: \\d+}/trayectos")
+    public Class<TrayectoResource> getTrayectoResource(@PathParam("viajesId") Long viajesId) {
+        if (logic.getViaje(viajesId) == null) {
+            throw new WebApplicationException("El recurso /viajes/" + viajesId + "/trayectos no existe.", 404);
+        }
+        return TrayectoResource.class;
+    }
+    
+    
+    
+    
     
     private List<ViajeDetailDTO> listViajesEntityToDTO(List<ViajeEntity> viajes){
         List<ViajeDetailDTO> viajesDTO = new ArrayList<ViajeDetailDTO>();
@@ -59,5 +119,9 @@ public class ViajeResource {
         }
         return viajesDTO;
     }
+    
+    
+    
+    
     
 }
