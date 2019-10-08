a209e70532ece6bca482e984c8e037da292faa61
Santiago <s.ballesteros@uniandes.edu.co>
2019-10-08 18:25:37 -0500

    commit viajeros

--- s1_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/dtos/ViajeroDetailDTO.java
+++ s1_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/dtos/ViajeroDetailDTO.java
@@ -5,12 +5,60 @@
  */
 package co.edu.uniandes.csw.carpooling.dtos;
 
+import co.edu.uniandes.csw.carpooling.entities.ReservaEntity;
+import co.edu.uniandes.csw.carpooling.entities.ViajeroEntity;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Santiago Ballesteros
  */
 public class ViajeroDetailDTO extends ViajeroDTO implements Serializable{
+    
+    private List<ReservaDTO> reservas;
+
+     /**
+     * Constructor vacio que llama a super
+     */
+    public ViajeroDetailDTO(){
+        super();
+    }
+    
+    /**
+     * Constructor para transformar un Entity a un DTO
+     *
+     * @param editorialEntity La entidad del Viajero para transformar a DTO.
+    */ 
+    public ViajeroDetailDTO(ViajeroEntity viajeroEntity){
+        super(viajeroEntity);
+        if(viajeroEntity != null){
+            if(viajeroEntity.getReservas()!=null){
+                reservas = new ArrayList<>();
+                for (ReservaEntity entityReserva : viajeroEntity.getReservas()){
+                    reservas.add(new ReservaDTO(entityReserva));
+                }
+            }
+        }
+    }
+    
+    /**    
+     * Transformar un DTO a un Entity
+     *
+     * @return El DTO de la editorial para transformar a Entity
+     */
+    @Override
+    public ViajeroEntity toEntity() {
+        ViajeroEntity viajeroEntity = super.toEntity();
+        if (reservas != null) {
+            List<ReservaEntity> reservaEntity = new ArrayList<>();
+            for (ReservaDTO dtoReserva : reservas) {
+                reservaEntity.add(dtoReserva.toEntity());
+            }
+            viajeroEntity.setReservas(reservaEntity);
+        }
+        return viajeroEntity;
+    }
     
 }
--- s1_carpooling-back/src/main/java/co/edu/uniandes/csw/carpooling/entities/ViajeroEntity.java
+++ s1_carpooling-back/src/main/java/co/edu/uniandes/csw/carpooling/entities/ViajeroEntity.java
@@ -6,7 +6,12 @@
 package co.edu.uniandes.csw.carpooling.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import uk.co.jemos.podam.common.PodamExclude;
 
 /**
  *
@@ -14,5 +19,22 @@ import javax.persistence.Entity;
  */
 @Entity
 public class ViajeroEntity extends UsuarioEntity implements Serializable {
+    @PodamExclude
+    @OneToMany(mappedBy = "viajeros", fetch = FetchType.LAZY)
+    private List<ReservaEntity> reservas = new ArrayList<ReservaEntity>();
+
+    /**
+     * @return the reservas
+     */
+    public List<ReservaEntity> getReservas() {
+        return reservas;
+    }
+
+    /**
+     * @param reservas the reservas to set
+     */
+    public void setReservas(List<ReservaEntity> reservas) {
+        this.reservas = reservas;
+    }
 
 }
