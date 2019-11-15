5f739ba275fc30619365e3def3b3e479582b7c73
Juan David Serrano <jd.serranom@uniandes.edu.co>
2019-11-15 13:59:14 -0500

    Se corrigieron duplicaciones y issues Viajes y Trayectos

--- s1_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/dtos/ViajeDTO.java
+++ s1_carpooling-api/src/main/java/co/edu/uniandes/csw/carpooling/dtos/ViajeDTO.java
@@ -120,76 +120,6 @@ public class ViajeDTO implements Serializable{
     }
 
     /**
-     * @return the destino
-     */
-    public String getDestino() {
-        return destino;
-    }
-
-    /**
-     * @param destino the destino to set
-     */
-    public void setDestino(String destino) {
-        this.destino = destino;
-    }
-
-    /**
-     * @return the origen
-     */
-    public String getOrigen() {
-        return origen;
-    }
-
-    /**
-     * @param origen the origen to set
-     */
-    public void setOrigen(String origen) {
-        this.origen = origen;
-    }
-
-    /**
-     * @return the fechaDeSalida
-     */
-    public Date getFechaDeSalida() {
-        return fechaDeSalida;
-    }
-
-    /**
-     * @param fechaDeSalida the fechaDeSalida to set
-     */
-    public void setFechaDeSalida(Date fechaDeSalida) {
-        this.fechaDeSalida = fechaDeSalida;
-    }
-
-    /**
-     * @return the fechaDeLlegada
-     */
-    public Date getFechaDeLlegada() {
-        return fechaDeLlegada;
-    }
-
-    /**
-     * @param fechaDeLlegada the fechaDeLlegada to set
-     */
-    public void setFechaDeLlegada(Date fechaDeLlegada) {
-        this.fechaDeLlegada = fechaDeLlegada;
-    }
-
-    /**
-     * @return the cupos
-     */
-    public Integer getCupos() {
-        return cupos;
-    }
-
-    /**
-     * @param cupos the cupos to set
-     */
-    public void setCupos(Integer cupos) {
-        this.cupos = cupos;
-    }
-
-    /**
      * @return the costoViaje
      */
     public Float getCostoViaje() {
@@ -248,5 +178,75 @@ public class ViajeDTO implements Serializable{
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+    
+    /**
+     * @return the fechaDeLlegada
+     */
+    public Date getFechaDeLlegada() {
+        return fechaDeLlegada;
+    }
+
+    /**
+     * @param fechaDeLlegada the fechaDeLlegada to set
+     */
+    public void setFechaDeLlegada(Date fechaDeLlegada) {
+        this.fechaDeLlegada = fechaDeLlegada;
+    }
+    
+        /**
+     * @return the fechaDeSalida
+     */
+    public Date getFechaDeSalida() {
+        return fechaDeSalida;
+    }
+
+    /**
+     * @param fechaDeSalida the fechaDeSalida to set
+     */
+    public void setFechaDeSalida(Date fechaDeSalida) {
+        this.fechaDeSalida = fechaDeSalida;
+    }
+    
+    /**
+     * @return the origen
+     */
+    public String getOrigen() {
+        return origen;
+    }
+
+    /**
+     * @param origen the origen to set
+     */
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+    
+    /**
+     * @return the destino
+     */
+    public String getDestino() {
+        return destino;
+    }
+
+    /**
+     * @param destino the destino to set
+     */
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    
+        /**
+     * @return the cupos
+     */
+    public Integer getCupos() {
+        return cupos;
+    }
+
+    /**
+     * @param cupos the cupos to set
+     */
+    public void setCupos(Integer cupos) {
+        this.cupos = cupos;
     }
 }
